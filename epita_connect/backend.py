690664b2c17ab33f76ba31537293b806ea51fd89
Marin Hannache <mareo@cri.epita.net>
2018-04-20 10:12:46 +0200

    backend: remove beta endpoint
    Signed-off-by: Marin Hannache <mareo@cri.epita.net>

--- epita_connect/backend.py
+++ epita_connect/backend.py
@@ -9,9 +9,5 @@ class EpitaOpenIdConnect(OpenIdConnectAuth):
     OIDC_ENDPOINT = 'https://intra.cri.epita.net/oidc'
 
     def oidc_config(self):
-        if self.setting('BETA', default=False):
-            endpoint = 'https://accounts-beta.cri.epita.net/oidc'
-        else:
-            endpoint = self.OIDC_ENDPOINT
-        return self.get_json(endpoint +
-                             '/.well-known/openid-configuration')
+        endpoint = self.OIDC_ENDPOINT
+        return self.get_json(endpoint + '/.well-known/openid-configuration')
